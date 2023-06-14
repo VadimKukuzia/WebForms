8cb77dc967dcf19305b41de4f64c6553178e4cca
jejkuzia <vadimol081@gmail.com>
2023-06-14 18:26:55 +0300

    upd admin, environmental

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from .models import User
 
+
+class UserAdminExportCSVMixin:
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+
 # Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin, UserAdminExportCSVMixin):
+    actions = ["export_as_csv"]
